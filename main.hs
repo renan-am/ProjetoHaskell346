65a2021f99f361ce90489f9151157e558fa7ff75
Renan <recam94@gmail.com>
2019-09-24 21:43:42 -0300

    add comentários

--- main.hs
+++ main.hs
@@ -1,4 +1,11 @@
+{--Projeto 1 MC346 - Haskell
+Nome: Adolf Pereira da Costa RA:164933
+Nome: Renan Clarindo Amorim  RA:186454
+--}
 
+
+--Função que recebe o input e quebra cada linha em elementos de uma lista e
+--posteriormente transforma cada palavra da linha em elementos de uma nova lista
 prepare a = split (lines a)
   where
     split x = split' x []
@@ -6,6 +13,7 @@ prepare a = split (lines a)
         split' [] res = res
         split' (x:xs) res = split' xs  (res ++ [(words x)])
 
+--Função que separa os pontos que são label em uma nova lista
 getClass [] = []
 getClass (x:xs)
   | x == [] = getClass' xs []
@@ -14,6 +22,8 @@ getClass (x:xs)
     getClass' [] res = res
     getClass' ([a,b]:xs) res = getClass' xs ((read b :: Int,[a]):res)
 
+
+--Função que separa os pontos que não são label em uma nova lista
 getCoord x = getCoord' x []
   where
     getCoord' [] res = res
@@ -21,8 +31,8 @@ getCoord x = getCoord' x []
       | x == [] = res
       | otherwise = getCoord' xs (x:res)
 
-getInt x = read x :: Int
-
+--Verifica se existem pontos da lista não label que já são classificados
+--e os coloca em uma lista de pontos
 check [] _ = False
 check ((_,x):xs) (a:_)
   | (check' x a) == True = True
@@ -33,10 +43,10 @@ check' (x:xs) a
   | x == a = True
   | otherwise = check' xs a
 
-
 {--
-    a = Elementos sem Label (NoLabel)
-    b = Elementos com Label (Label)
+    a = lista de elementos sem Label (NoLabel)
+    b = lista de elementos com Label (Label)
+    Função que retorna a distância ao quadrado entre dois pontos
 --}
 getDist a b = [dist x y | x <- a, y <- b]
   where
@@ -44,7 +54,7 @@ getDist a b = [dist x y | x <- a, y <- b]
       where
         sqDiff a b = ((read a :: Float)-(read b :: Float))**2
 
-
+--Função que adiciona o nome do ponto dentro da lista de labels
 addLabel a b = addLabel' a b []
   where 
     addLabel' [] _ res = res
@@ -52,12 +62,14 @@ addLabel a b = addLabel' a b []
       | (check' x b) == True = res ++ ((y,(a:x)):xs)
       | otherwise = addLabel' xs (a,b,c) ((y,x):res)
 
-compara (a,b,c) (x,y,z) = if c < z then
-  (a,b,c)
-  else
-  (x,y,z)
-
+--Função que verifica qual o ponto de menor distância em relação a um label
+--e retorna uma tupla com ponto, nome do label e a distância
 checkLabel x = foldl1 (compara) x
+  where 
+    compara (a,b,c) (x,y,z) = if c < z then
+      (a,b,c)
+      else
+      (x,y,z)
 
 getLabel c cl = [x | x <- c, (check cl x)]
 getNoLabel c cl = [x | x <- c, not(check cl x)]
@@ -71,15 +83,17 @@ classify c cl = classify' c cl (getLabel c cl) (getNoLabel c cl)
 
 
 
-
+--Função que ordena as tuplas de acordo com o label de forma crescente
 orderByGroup [] = []
 orderByGroup (x:xs) = orderByGroup menores ++ [x] ++ orderByGroup maiores
   where  
     menores = [y | y <- xs, compara' y x]
     maiores = [y | y <- xs, not(compara' y x)]
-        
-compara' (a,_) (b,_) = a <= b 
 
+    compara' (a,_) (b,_) = a <= b
+
+
+--Função que ordena as tuplas de acordo com o os nomes dos labels de forma alfabética  
 orderByElem [] = []
 orderByElem ((a,b):xs) = ((a,(orderByElem' b)):orderByElem xs)
 
@@ -94,18 +108,8 @@ orderByElem' (x:xs) = orderByElem' less ++ [x] ++ orderByElem' more
 main = do 
   txt <- getContents
   let dat = prepare txt
-
   let classes = getClass dat 
-  let coord = getCoord dat
-  
+  let coord = getCoord dat  
   let res = orderByElem(orderByGroup (classify coord classes))
 
   mapM_ print (res)
-
-
-
-  -- let aux = orderByElem res
-  -- print aux
-
-  -- let res2 =  addLabel classes ((checkLabel  (getDist (getNoLabel coord classes) (getLabel coord classes))))
-  -- print res2
